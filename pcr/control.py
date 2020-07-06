a8abaa6ca0b7a4af045f8981f2fc18da62a2987e
wiio <765874234@qq.com>
2020-07-06 20:33:33 +0800

    add pcr fight dungeon

--- pcr/control.py
+++ pcr/control.py
@@ -8,7 +8,7 @@ def run_pcr_fight(fight_time=100, use_stone=0):
     loger.log("start fight")
     while True:
         #ret = fgo.daliy.daliy_free_draw()
-        ret = fight.fight(fight_time=fight_time, use_stone=use_stone)
+        ret = fight.fight_dungeon(fight_time=fight_time, use_stone=use_stone)
         if ret != 0:
             loger.log("pcr Fight Failed...")
             break
--- pcr/fight.py
+++ pcr/fight.py
@@ -4,19 +4,19 @@ from utility.utility import *
 from utility.loger import log
 
 
-def fight(fight_time=100, use_stone=0):
+def fight_event(fight_time=15, use_stone=0):
     while True:
         flag = False
         fight_count = 0
-        s, p = match_until(["next_fight", 'start_fight', 'next', 'shop', 'next_1', 'close'], require_coor=True, cof=0.85)
+        s, p = match_until(["next_fight", 'start_fight', 'start_fight_2', 'next', 'shop', 'next_1', 'close', 'ap_out'], require_coor=True, cof=0.85)
         if s == 'next_fight':
             tap(p[0], p[1] + 50)
             flag = True
-        elif s == 'start_fight':
-            tap(1114, 607, w=1)
+        elif s == 'start_fight' or s == 'start_fight_2':
             tap(1114, 607, w=1)
             if flag:
                 fight_count += 1
+                flag = False
         elif s == 'next':
             tap(1107, 671, w=4)
         elif s == 'shop':
@@ -25,6 +25,47 @@ def fight(fight_time=100, use_stone=0):
             tap(1107, 671, w=4)
         elif s == 'close':
             tap(p[0], p[1])
+        elif s == 'ap_out':
+            if use_stone > 0:
+                tap(786, 494, w=1)
+                tap(786, 494, w=1)
+                use_stone -= 1
+            else:
+                log("体力完了")
+                return 0
+
+        if fight_count > fight_time:
+            return 0
+
+def fight_dungeon(fight_time=15, use_stone=0):
+    while True:
+        flag = False
+        fight_count = 0
+        s, p = match_until(["next_fight_d", 'start_fight_d', 'start_fight_2', 'next', 'shop', 'next_1', 'close', 'ap_out','ok_d'], require_coor=True, cof=0.85)
+        if s == 'next_fight_d':
+            tap(p[0], p[1])
+            flag = True
+        elif s == 'start_fight_d' or s == 'start_fight_2':
+            tap(1114, 607, w=1)
+            if flag:
+                fight_count += 1
+                flag = False
+        elif s == 'next':
+            tap(1107, 671, w=4)
+        elif s == 'shop':
+            tap(498, 492, w=1)
+        elif s == 'next_1':
+            tap(1107, 671, w=4)
+        elif s == 'close' or s == 'ok_d':
+            tap(p[0], p[1])
+        elif s == 'ap_out':
+            if use_stone > 0:
+                tap(786, 494, w=1)
+                tap(786, 494, w=1)
+                use_stone -= 1
+            else:
+                log("体力完了")
+                return 0
 
         if fight_count > fight_time:
             return 0
